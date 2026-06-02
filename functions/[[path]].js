451f853498183058ece26b707b1ce3ae15786b4b
lebahhack <lebahhack@gmail.com>
2026-06-03 00:20:48 +0700

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,37 +1,34 @@
 export async function onRequest(context) {
 
   const url = new URL(context.request.url);
-  const path = url.pathname
-    .replace(/^\/+/, "")
-    .replace(/\/+$/, "");
+  const path = url.pathname.replace(/^\/+/, "");
+
+  // biarkan file statis lewat
+  if (
+    path.includes(".") ||
+    path.startsWith("css/") ||
+    path.startsWith("js/") ||
+    path.startsWith("assets/")
+  ) {
+    return context.env.ASSETS.fetch(context.request);
+  }
 
   const reserved = [
     "",
     "login",
     "register",
     "dashboard",
-    "profile",
-    "api",
-    "css",
-    "js",
-    "assets",
-    "favicon.ico",
-    "robots.txt",
-    "sitemap.xml"
+    "profile"
   ];
 
+  // username
   if (!reserved.includes(path)) {
-
     return context.env.ASSETS.fetch(
       new Request(
         new URL("/profile.html", url)
       )
     );
-
   }
 
-  return context.env.ASSETS.fetch(
-    context.request
-  );
-
+  return context.env.ASSETS.fetch(context.request);
 }
