f5c5d5cc35fad7ce2e9be8b04b47ba7a6b8ad310
lebahhack <lebahhack@gmail.com>
2026-06-03 11:41:52 +0700

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,17 +1,3 @@
-if (window.__PROFILE__) {
-
-  profile =
-    window.__PROFILE__;
-
-  renderProfile();
-  renderLinks();
-
-}
-else {
-
-  initProfile();
-
-}
 
 /* =========================
    PROFILE PAGE
